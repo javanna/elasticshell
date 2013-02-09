a5de85a840e22a579da93977a7ea88a5e836eac8
Luca Cavanna <luca.cavanna@trifork.nl>
2013-02-09 12:56:58 +0100

    added executable files to assembly, replacing project name through maven aseembly plugin

--- bin/elasticshell.bat
+++ bin/elasticshell.bat
@@ -4,13 +4,10 @@ SETLOCAL
 if DEFINED JAVA_HOME set JAVA=%JAVA_HOME%/bin/java
 if NOT DEFINED JAVA_HOME set JAVA=java
 
-set APP_NAME=elasticshell
-set VERSION=0.1-SNAPSHOT
-
 set SCRIPT_DIR=%~dp0
 for %%I in ("%SCRIPT_DIR%..") do set DIRNAME=%%~dpfI
 cd %DIRNAME%
 
-%JAVA% %JAVA_OPTS% -jar lib/%APP_NAME%-%VERSION%.jar
+%JAVA% %JAVA_OPTS% -jar lib/${project.build.finalName}.jar
 
 ENDLOCAL

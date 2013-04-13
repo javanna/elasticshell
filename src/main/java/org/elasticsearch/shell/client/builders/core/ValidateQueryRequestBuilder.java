78760c2573e93a960ce68b214906c8fc86074211
Luca Cavanna <luca.cavanna@trifork.nl>
2013-04-13 17:35:59 +0200

    renamed query to queryBuilder for consistency

--- src/main/java/org/elasticsearch/shell/client/builders/core/ValidateQueryRequestBuilder.java
+++ src/main/java/org/elasticsearch/shell/client/builders/core/ValidateQueryRequestBuilder.java
@@ -18,6 +18,9 @@
  */
 package org.elasticsearch.shell.client.builders.core;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.admin.indices.validate.query.QueryExplanation;
 import org.elasticsearch.action.admin.indices.validate.query.ValidateQueryRequest;
@@ -29,9 +32,6 @@ import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.shell.client.builders.AbstractRequestBuilderJsonOutput;
 import org.elasticsearch.shell.json.JsonToString;
 import org.elasticsearch.shell.json.StringToJson;
-
-import java.io.IOException;
-import java.util.List;
 
 import static org.elasticsearch.rest.action.support.RestActions.buildBroadcastShardsHeader;
 
@@ -57,7 +57,7 @@ public class ValidateQueryRequestBuilder<JsonInput, JsonOutput> extends Abstract
         return this;
     }
 
-    public ValidateQueryRequestBuilder<JsonInput, JsonOutput> query(QueryBuilder queryBuilder) {
+    public ValidateQueryRequestBuilder<JsonInput, JsonOutput> queryBuilder(QueryBuilder queryBuilder) {
         request.query(queryBuilder);
         return this;
     }
